072858cb29cef729e4d8d079bfe2508b31c458b7
Jack Xu <34068669+thejackxu@users.noreply.github.com>
2020-12-05 02:55:29 -0500

    Established fundamental database setup, essential tables, and necessary methods to add to tables
    Resolved syntax issues pertaining to table setup. Resolved foreign key enable set up. Basic backend set up complete.

--- Database.py
+++ Database.py
@@ -5,36 +5,99 @@ class DBManager:
     def __init__(self):
         self.conn = None
 
-    def setUpDB(self):
-        try:
-            dbName = 'Property_Management.db'
-            self.conn = sqlite3.connect(dbName)
-
-        except Error as e:
-            print('Error occurred: {0}'.format(e))
-
     def createTable(self):
         try:
             #primary property id & address
             pidTable = '''
-                        CREATE TABLE IF NOT EXIST property_id(
+                        CREATE TABLE IF NOT EXISTS pidTable(
                         primary_id TEXT PRIMARY KEY UNIQUE NOT NULL,
                         address TEXT NOT NULL
-                        )             
+                        )       
                         '''
-
+            #cash inflow table, property ID, date, $ amount, payment category (rent, late fee, etc)
             cashInflowTable = '''
-                        CREATE TABLE IF NOT EXIST cashInflow(
+                        CREATE TABLE IF NOT EXISTS cashInflowTable(
                         primary_id TEXT UNIQUE NOT NULL,
+                        paymentDate TEXT NOT NULL,
+                        dollarAmount MONEY NOT NULL,
+                        paymentCategory TEXT NOT NULL,
                         FOREIGN KEY (primary_id)
-                            REFERENCES pidTable (primary_id),
-                        date TEXT NOT NULL,
-                        dollarAmount money NOT NULL,
-                        paymentCategory text NOT NULL
+                            REFERENCES pidTable (primary_id)
+                            ON DELETE SET NULL
+                            ON UPDATE CASCADE
+                        )
+                        '''
+            #cash outflow table, property ID, date, $ amount, payment category (rent, late fee, etc), payment category
+            #others, and payment type (how did they pay)
+            cashOutflowTable = '''
+                        CREATE TABLE IF NOT EXISTS cashOutflowTable(
+                        primary_id TEXT UNIQUE NOT NULL,
+                        paymentDate TEXT NOT NULL,
+                        dollarAmount MONEY NOT NULL,
+                        paymentCategory TEXT NOT NULL,
+                        paymentCategoryOthers TEXT NOT NULL,
+                        paymentType TEXT NOT NULL,
+                        FOREIGN KEY (primary_id)
+                            REFERENCES pidTable (primary_id)
+                            ON DELETE SET NULL
+                            ON UPDATE CASCADE          
                         )
                         '''
 
+            reader = self.conn.cursor()
+            reader.execute(pidTable)
+            reader.execute(cashInflowTable)
+            reader.execute(cashOutflowTable)
 
+        except sqlite3.Error as e:
+            print('Error occurred: {0}'.format(e))
 
-        except Error as e:
+    def setUpDB(self):
+        try:
+            dbName = 'Property_Management.db'
+            self.conn = sqlite3.connect(dbName)
+            print('Database successfully connected!')
+            self.conn.execute('PRAGMA foreign_keys = 1')
+            self.createTable()
+            #return True
+
+        except sqlite3.Error as e:
+            self.conn.close()
+            print('Error occurred: {0}'.format(e))
+            #return False
+
+    def addProperties(self, PID, newaddress):
+        query = '''
+                INSERT INTO pidTable VALUES ('{0}', '{1}')'''.format(PID, newaddress)
+        try:
+            reader = self.conn.cursor()
+            reader.execute(query)
+            self.conn.commit()
+            print('Property at {0} added as {1}'.format(newaddress, PID))
+
+        except sqlite3.IntegrityError as e:
+            print('Error occurred: {0}'.format(e))
+
+    def addCashInflow(self, PID, Date, Amount, Type):
+        query = '''
+                INSERT INTO cashInflowTable VALUES ('{0}', '{1}', '{2}', '{3}')'''.format(PID, Date, Amount, Type)
+        try:
+            reader = self.conn.cursor()
+            reader.execute(query)
+            self.conn.commit()
+            print('Payment (Inflow) successfully added to property ID {0}'.format(PID))
+
+        except sqlite3.IntegrityError as e:
+            print('Error occurred: {0}'.format(e))
+
+    def addCashOutflow(self, PID, Date, Amount, Type, TypeOthers, PaymentType):
+        query = '''
+                INSERT INTO cashOutflowTable VALUES ('{0}', '{1}', '{2}', '{3}', '{4}', '{5}')'''.format(PID, Date, Amount, Type, TypeOthers, PaymentType)
+        try:
+            reader = self.conn.cursor()
+            reader.execute(query)
+            self.conn.commit()
+            print('Payment (Outflow) successfully added to property ID {0}'.format(PID))
+
+        except sqlite3.IntegrityError as e:
             print('Error occurred: {0}'.format(e))
---
+++ test.py
@@ -0,0 +1,8 @@
+import Database
+from Database import DBManager
+
+manager = DBManager()
+manager.setUpDB()
+manager.addProperties('A', '1401 Ridgecrest Dr., Plano, TX 75074')
+manager.addCashInflow('B', '12/05/2020', 1500, 'Rent')
+manager.addCashOutflow('A', '12/05/2020', 400, 'Maintenance', '', 'Checking 4147')
